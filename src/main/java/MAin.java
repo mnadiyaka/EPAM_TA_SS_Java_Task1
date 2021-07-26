fb4d5d7c8797fddb0aa57b7606e73f2a479af481
Nadiia <mnadiyaka@gmail.com>
2021-07-26 15:28:10 +0300

    Exceptions made. PArt of fibonachi is working

--- src/main/java/MAin.java
+++ src/main/java/MAin.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MAin {
@@ -24,12 +25,28 @@ public class MAin {
         interval(m);
         System.out.println(m.getA() + " " + m.getB());
         summ(m);
+        fibonachi(m);
     }
 
     public static void interval(MAin m) {
-        Scanner sc = new Scanner(System.in);
-        m.setA(sc.nextInt());
-        m.setB(sc.nextInt());
+        int i =0;
+        for ( i = 1; i <= 3; i++) {
+            System.out.println("Enter a, b: ");
+            try {
+                Scanner sc = new Scanner(System.in);
+                m.setA(sc.nextInt());
+                m.setB(sc.nextInt());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Enter 2 numbers separately. You have " + (3-i) + " try");
+                continue;
+            }
+        }
+        if (i == 3){
+            System.out.println("Stop toying around. Go away");
+            System.exit(0);
+        }
+
     }
 
     public static void summ(MAin m) {
@@ -43,5 +60,43 @@ public class MAin {
         }
 
         System.out.println("Summ odd = " + s_odd + "; Summ even = " + s_even);
+    }
+
+    public static void fibonachi(MAin m) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        int kE = 0;
+        int kO = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a < m.getA() && a % 2 == 0) {
+                kE++;
+                i++;
+                System.out.print(a + " ");
+            }
+            if (b < m.getA() && b % 2 == 0) {
+                kE++;
+                i++;
+                System.out.print(b + " ");
+            }
+            if (a < m.getB() && a % 2 != 0) {
+                kO++;
+                i++;
+                System.out.print(a + " ");
+            }
+            if (b < m.getB() && b % 2 != 0) {
+                kO++;
+                i++;
+                System.out.print(b + " ");
+            }
+            a = a + b;
+            b = a + b;
+        }
+
     }
 }
