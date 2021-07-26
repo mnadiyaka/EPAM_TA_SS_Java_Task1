8ff965060c9e6b1d0cb183e59d722cd0713e9e1d
Nadiia <mnadiyaka@gmail.com>
2021-07-26 18:13:23 +0300

    sort partially working

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -1,8 +1,6 @@
 package task2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
@@ -10,22 +8,39 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("choose smth from list: ");
-        for (Opt option: Opt.values()){
+        for (Opt option : Opt.values()) {
             System.out.print(option + " ");
         }
         String selected = sc.next().toUpperCase();
-
-        switch (Opt.valueOf(selected)) {
-            case ADD -> {
-                System.out.println("What to add BMV(1) or Mersedes (2): ");
-                int a = sc.nextInt();
-                switch (a) {
-                    case 1 -> cars.add(new Bmw(sc.next(), sc.next()));
-                    case 2 -> cars.add(new Mersedes(sc.next(), sc.next()));
+        while (true) {
+            switch (Opt.valueOf(selected)) {
+                case ADD -> {
+                    System.out.println("What to add BMV(1) or Mersedes (2): ");
+                    int a = sc.nextInt();
+                    switch (a) {
+                        case 1 -> cars.add(new Bmw(sc.next(), sc.next()));
+                        case 2 -> cars.add(new Mersedes(sc.next(), sc.next()));
+                    }
                 }
-            }
-            case REMOVE ->{
+                case REMOVE -> {
 
+                }
+                case SHOW -> {
+                    for (Car car : cars) {
+                        System.out.println(car.toString());
+                    }
+                }
+                case SORT -> {
+                    cars.sort(new Comparator<Car>() {
+                        @Override
+                        public int compare(Car o1, Car o2) {
+                            return o1.getClass().toString().compareTo(o2.getClass().toString()) + o1.getBrand().compareTo(o2.getBrand());
+                        }
+                    });
+                    for (Car car : cars) {
+                        System.out.println(car.toString());
+                    }
+                }
             }
         }
     }
@@ -35,13 +50,13 @@ enum Opt {
     ADD, REMOVE, SHOW, SORT;
 }
 
-class Bmw extends Car{
+class Bmw extends Car {
     private String brand;
     private String model;
 
-    public Bmw(String brand, String model){
-        this.brand=brand;
-        this.model=model;
+    public Bmw(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
     }
 
     @Override
@@ -59,16 +74,24 @@ class Bmw extends Car{
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Bmw{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
     }
 }
 
-class Mersedes extends Car{
+class Mersedes extends Car {
     private String brand;
     private String model;
 
-    public Mersedes(String brand, String model){
-        this.brand=brand;
-        this.model=model;
+    public Mersedes(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
     }
 
     @Override
@@ -86,21 +109,32 @@ class Mersedes extends Car{
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Mersedes{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
     }
 }
 
 interface Alarm {
-    default String turnAlarmOn(){
+    default String turnAlarmOn() {
         return "AAAAAAA";
     }
-    default String  turnAlarmOff(){
+
+    default String turnAlarmOff() {
         return "-------";
     }
 }
 
 interface Vehicle {
     String getBrand();
+
     boolean speedUp();
+
     boolean slowDown();
 }
 
