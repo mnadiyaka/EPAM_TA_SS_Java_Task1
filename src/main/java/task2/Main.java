d3cf4535bc9d1469ca21f2213f3e702536d30abd
Nadiia <mnadiyaka@gmail.com>
2021-07-26 18:00:19 +0300

    add is working

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -1,8 +1,33 @@
 package task2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
+        List<Car> cars = new ArrayList<>();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choose smth from list: ");
+        for (Opt option: Opt.values()){
+            System.out.print(option + " ");
+        }
+        String selected = sc.next().toUpperCase();
+
+        switch (Opt.valueOf(selected)) {
+            case ADD -> {
+                System.out.println("What to add BMV(1) or Mersedes (2): ");
+                int a = sc.nextInt();
+                switch (a) {
+                    case 1 -> cars.add(new Bmw(sc.next(), sc.next()));
+                    case 2 -> cars.add(new Mersedes(sc.next(), sc.next()));
+                }
+            }
+            case REMOVE ->{
+
+            }
+        }
     }
 }
 
